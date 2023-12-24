715458d969b5405330b7ce177942d78ce09a33aa
JeiversonChristian <jeiversonc@gmail.com>
2023-12-24 11:38:36 -0300

    fazendo o quadrado se movimentar e novamente corrigindo o canvas

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width;
-const CANVAS_HEIGHT = canvas.heigth;
+const CANVAS_WIDTH = 600;
+const CANVAS_HEIGHT = 600;
 
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
 
+let x = 0;
 ctx.fillRect(50, 50, 100, 100);
 
 function animate(){
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillRect(50, 50, 100, 100);
+    ctx.clearRect(0, 0, 600, 600);
+    ctx.fillRect(x, 50, 100, 100);
+    x++;
     requestAnimationFrame(animate);
 }
 
