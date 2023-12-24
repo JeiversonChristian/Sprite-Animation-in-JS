f06935ec30585f654f3290bf5dbbe3306de2b712
JeiversonChristian <jeiversonc@gmail.com>
2023-12-24 11:30:57 -0300

    corrigindo detalhe na criação do quadrado

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 600;
-const CANVAS_HEIGHT = canvas.heigth = 600;
+const CANVAS_WIDTH = canvas.width;
+const CANVAS_HEIGHT = canvas.heigth;
 
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
@@ -9,7 +9,7 @@ playerImage.src = 'shadow_dog.png';
 ctx.fillRect(50, 50, 100, 100);
 
 function animate(){
-    ctx.clearRect(0, 0, 600, 600);
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.fillRect(50, 50, 100, 100);
     requestAnimationFrame(animate);
 }
